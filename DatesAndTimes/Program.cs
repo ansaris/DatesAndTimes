2837f140757915dd6d5f25acb2d8ae792f5cf075
ansaris <salman.ansari.usa@gmail.com>
2016-04-26 12:16:00 -0500

    Finish lesson.

--- DatesAndTimes/Program.cs
+++ DatesAndTimes/Program.cs
@@ -11,8 +11,25 @@ namespace DatesAndTimes
         static void Main(string[] args)
         {
             DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+            //Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+            //Console.WriteLine(myValue.ToLongDateString());
+            //Console.WriteLine(myValue.ToLongTimeString());
 
-            Console.WriteLine(myValue);
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            //Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            //Console.WriteLine(myValue.AddHours(-3).ToLongTimeString());
+            //Console.WriteLine(myValue.Month);
+
+            //DateTime myBirthday = new DateTime(1969, 12, 7);
+            //Console.WriteLine(myBirthday.ToShortDateString());
+
+            DateTime myBirthday = DateTime.Parse("12/7/1969");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine("Birthday: {0}\nAge: {1}\nDays alive: {2}", myBirthday, myAge, myAge.TotalDays);
+
             Console.ReadLine();
         }
     }
